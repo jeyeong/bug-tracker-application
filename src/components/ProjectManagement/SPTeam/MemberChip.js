00247634d5ee27c0a9eb292aef4a45608245caf0
Je Yeong - Pycharm <jeyeong@uchicago.edu>
2021-12-27 18:11:23 -0600

    Added button to delete member

--- src/components/ProjectManagement/SPTeam/MemberChip.js
+++ src/components/ProjectManagement/SPTeam/MemberChip.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 const MemberChip = ({ type }) => {
+  const deleteMember = () => {
+    console.log('delete');
+  }
+
   return (
     <div className='projmgmt-s-team__chip' id={`chip__${type ?? 'submitter'}`}>
+      <span
+        className='projmgmt-s-team__delete-member'
+        onClick={deleteMember}
+      >
+        x
+      </span>
       Je Yeong Soh
     </div>
   );
